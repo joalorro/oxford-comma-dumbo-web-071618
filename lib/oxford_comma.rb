d0d98576e244c6a38a261a45a469ec4ae9db0d17
Josue V. Alorro <joalorro@gmail.com>
2018-07-02 20:50:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ veggies = ['okra', 'bok choy', 'kale']
 def oxford_comma(array)
   last = array.last
   array.pop
-  array.join(', ')
+  sen = array.join(', ')
   
-  return "#{array}, and #{last}"
+  return "#{sen}, and #{last}"
 end
 
 puts oxford_comma(veggies)
